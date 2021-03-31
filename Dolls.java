74768c241f068009313a3fdf5fec9bbcaf04d326
ahncho <ahncho8880@gmail.com>
2021-04-01 02:17:49 +0900

    Dolls update

--- Dolls.java
+++ Dolls.java
@@ -1,43 +1,30 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Dolls {
-    static int N, K;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        N = input.nextInt();
-        K = input.nextInt();
+        int N = input.nextInt();
+        int k = input.nextInt();
         int[] dolls = new int[N];
-        int sum=0;
-        for(int i = 0;i<N;i++){
+        double min = -1;
+        for(int i=0;i<dolls.length;i++)
             dolls[i] = input.nextInt();
-            sum+=dolls[i];
-        }
-        double min=-1;
-        for(int k=K;k<=N;k++){
-            int start =0;
-            while(start+K <= N){
-                int[] dollSds = new int[K];
-                int sum2=0;
-                int n=0;
-                for(int i=start;i<K+start;i++){
-                    dollSds[n++] = dolls[i]; 
-                    sum2 += dolls[i];
+        
+        while(k<=N){
+            for(int i=0;i<=N-k;i++){
+                double m=0;
+                for(int j=0 ;j<k;j++)
+                    m+=dolls[j+i];
+                m /= k;
+                double mm=0;
+                for(int j=0 ;j<k;j++){
+                    mm+=(dolls[i+j]-m)*(dolls[i+j]-m);
                 }
-                float m = (float)sum2/K;
-                double tmp = sd(dollSds, m);
-                min = (Double.compare(min, -1)==0 || min>tmp) ? tmp : min;
-                start++;
+                mm = Math.sqrt(mm/k);
+                min = Double.compare(-1, min)!=0 ? Math.min(min, mm) : mm;
             }
+            k++;
         }
-        min = Double.parseDouble(String.format("%.11f", min));
         System.out.println(min);
-    }
-    private static double sd(int[] dollSds, float m) {
-        double sum = 0;
-        for(int i=0;i<K;i++){
-            sum+= Math.pow((dollSds[i]-m),2);
-        }
-        return Math.sqrt(sum/K);
     }
 }
