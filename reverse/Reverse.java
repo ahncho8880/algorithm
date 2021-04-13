5fb4e0f575bee3c2a9d11ffd5ae85942615b5826
ahncho <ahncho8880@gmail.com>
2021-04-13 22:48:18 +0900

    update reverse

--- reverse/Reverse.java
+++ reverse/Reverse.java
@@ -1,10 +1,12 @@
 package reverse;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reverse {
     static int N, M;
+    static int ANS=1 ,MOD=1000000007;
+    static ArrayList<String[][]> list = new ArrayList<String[][]>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
@@ -13,72 +15,32 @@ public class Reverse {
         for(int i=0;i<N;i++){
             boards[i] = sc.next().split("");
         }
-        doPick(boards);
-        
+        doReverse(boards);
     }
-    private static void doPick(String[][] boards) {
+    private static void doReverse(String[][] boards) {
+        int cnt = 0;
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-                System.out.print(boards[i][j]+"");
-            }
-            System.out.println();
-        }
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(flag(i,j, boards)){
-                    System.out.println(i+", "+j);
-                    doReverse(boards, i, j);           
-                    doPick(boards);
-                    doReverse(boards, i, j);
+                if(isPossible(boards, i, j)){
+                    cnt++;
                 }
             }
         }
+        for(int i=0;i<cnt;i++){
+            ANS*=2%MOD;
+        }
+        System.out.println(ANS);
     }
-    private static String[][] doReverse(String[][] boards, int i, int j){
-        if(boards[i][j].equals("W")){
-            boards[i][j]="B";
-            if(i+1<N){
-                boards[i+1][j] = "B";
+    private static boolean isPossible(String[][] boards, int i, int j) {
+        int[] x = {1,-1,0,0};
+        int[] y = {0,0,1,-1};
+        char color = boards[i][j].charAt(0);
+        for(int k=0;k<4;k++){
+            int tmpX = i+x[k];
+            int tmpY = j+y[k];
+            if(!(tmpX<0 || tmpX>=N || tmpY<0 || tmpY>=M) && color!=boards[tmpX][tmpY].charAt(0)){
+                return false;
             }
-            if(i-1>=0){
-                boards[i-1][j] = "B";
-            }
-            if(j+1<M){
-                boards[i][j+1] = "B";
-            }
-            if(j-1>=M){
-                boards[i][j-1] = "B";
-            }
-        }else{
-            boards[i][j]="W";
-            if(i+1<N){
-                boards[i+1][j] = "W";
-            }
-            if(i-1>=0){
-                boards[i-1][j] = "W";
-            }
-            if(j+1<M){
-                boards[i][j+1] = "W";
-            }
-            if(j-1>=M){
-                boards[i][j-1] = "W";
-            }
-        }
-        return boards;
-    }
-    private static boolean flag(int x, int y, String[][] boards) {
-        String res = boards[x][y];
-        if(x+1<N && !boards[x+1][y].equals(res)){
-            return false;
-        }
-        if(x-1>=0 && !boards[x-1][y].equals(res)){
-            return false;
-        }
-        if(y+1<M && !boards[x][y+1].equals(res)){
-            return false;
-        }
-        if(y-1>=0 && !boards[x][y-1].equals(res)){
-            return false;
         }
         return true;
     }
