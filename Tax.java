9cb66eb186b28d35d0e9e25050c2bad86c07823d
unknown <ahncho8880@gmail.com>
2021-04-14 20:54:02 +0900

    update www

--- Tax.java
+++ Tax.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Tax {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
         if(n%1000==0 && n<=10000000){
             System.out.println((int)(n*0.78)+" "+(int)(n - n*0.2*0.22));
         }
