38f10c66406f822e32d9e411ccf33c354d9a0449
unknown <ahncho8880@gmail.com>
2021-04-14 16:57:14 +0900

    rename samsungSW

---
+++ SamsungSW/ConveyorBelt.java
@@ -0,0 +1,67 @@
+package samsungSW;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConveyorBelt {
+    static int N,K;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        int[] durability = new int[N*2];
+        boolean[] onRobot = new boolean[N];
+        for(int i=0;i<N*2;i++){
+            durability[i] = sc.nextInt();
+        }
+        rotateConveyor(onRobot, durability,0,0);
+    }
+    private static void rotateConveyor(boolean[] robot, int[] durability, int useless,int cnt){
+        if(K<=useless){
+            System.out.println(cnt);
+            return;
+        }
+        int temp=0;
+        useless = 0;
+        for(int i=2*N-1;i>=0;i--){
+            if(i==0){
+                durability[i] = temp;
+                System.out.println(Arrays.toString(durability));
+                System.out.println(Arrays.toString(robot));
+                moveRobot(robot,durability);
+                System.out.println(Arrays.toString(durability)+"*");
+                System.out.println(Arrays.toString(robot)+"*");
+                if(durability[i]>0){
+                    durability[i]-=1;
+                    robot[i] = true;
+                }
+            }else if(i==2*N-1){
+                temp = durability[i];
+                durability[i] = durability[i-1];
+            }else if(i==N-1){
+                robot[i] = false;
+                durability[i] = durability[i-1];
+            }else{
+                durability[i] = durability[i-1];
+                // robot[i] = robot[i-1];
+            }
+        }
+        
+        for(int dura : durability){
+            useless += dura==0 ? 1 : 0;
+        }
+        rotateConveyor(robot, durability, useless, cnt+1);
+    }
+    private static void moveRobot(boolean[] robot, int[] durability){
+        for(int i=N-1;i>=0;i--){
+            if(robot[i]){
+                int k = i+1;
+                while(k<N && durability[k]>0 && !robot[k]){
+                    robot[k-1] = false;
+                    durability[k] -= 1;
+                    robot[k++] = true;
+                }
+            }
+        }
+    }
+}
