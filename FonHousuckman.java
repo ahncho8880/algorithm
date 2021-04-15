bb8d1632051894b7ff79dc3a2fa5d75eb9e216f9
unknown <ahncho8880@gmail.com>
2021-04-15 20:58:18 +0900

    update housuckman

---
+++ FonHousuckman.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FonHousuckman {
+    static int X, A, B;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        X = Integer.parseInt(br.readLine());
+        A = Integer.parseInt(br.readLine());
+        B = Integer.parseInt(br.readLine());
+    }    
+}
